d8813de8b4ad47d1a7b51b2df9f74ef7003cdb52
Marcelo Garay <marcelo.garay@overactive.com>
2021-06-20 22:16:23 -0400

    Update metodo for Conjure

--- src/main/java/edu/alenasoft/gildedrose/Conjure.java
+++ src/main/java/edu/alenasoft/gildedrose/Conjure.java
@@ -4,16 +4,12 @@ public class Conjure implements QualityItem{
     @Override
     public void updateQuality(Item item) {
         if (item.getQuality() > 0) {
-            item.setQuality(item.getQuality() - 1);
+            item.setQuality(item.getQuality() - 2);
             if (item.getSellIn() < 0 && item.getQuality() > 0) {
-                item.setQuality(item.getQuality() - 1);
+                item.setQuality(item.getQuality() - 2);
             }
         }
-        if (item.getQuality() > 0) {
-            item.setQuality(item.getQuality() - 1);
-            if (item.getSellIn() < 0 && item.getQuality() > 0) {
-                item.setQuality(item.getQuality() - 1);
-            }
-        }
+
+        if (item.getQuality() < 0) item.setQuality(0);
     }
 }
